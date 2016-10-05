2f5dd5cbfac26fd3120c249f9a475d3247f409e4
leileibill <leileibill@gmail.com>
2016-10-05 12:10:36 -0500

    well

--- gpib_measurement/dickson/data/plot_efficiency.m
+++ gpib_measurement/dickson/data/plot_efficiency.m
@@ -5,9 +5,10 @@ scale = 2;
 set_figure_style_pre();
 
 
-to_plot = { '0p5' 200 100; ...
-            '0p5' 250 100; ...
-            '0p5' 250 20; ...
+to_plot = { 50 '0p5' 250 100; ...
+            50 '0p5' 250 20; ...
+            90 '0p5' 250 20; ...
+            90 '0p8' 250 20; ...
             };
 x_axis = 'iout';
 y_axis = 'loss';
@@ -15,10 +16,8 @@ Marker = {'^-','o-','x-','s-','-+','.-'};
 num_to_plot = length(to_plot(:,1));
 legend_info = {};
 for index = 1:num_to_plot
-    to_plot{index,1}
-    to_plot{index,2}
-    to_plot{index,3}
-    file = sprintf('SC_Regulation_%s_%ik_%iOhm.dat',to_plot{index,1},to_plot{index,2},to_plot{index,3})
+
+    file = sprintf('SC_Regulation_%iV_%s_%ik_%iOhm.dat',to_plot{index,1},to_plot{index,2},to_plot{index,3},to_plot{index,4})
     data = csvread(file,1,0);
     iout = data(:,4);
     efficiency = data(:,7);
@@ -26,7 +25,7 @@ for index = 1:num_to_plot
 
     plot(iout,efficiency,Marker{index});
     hold on;
-    legend_info{index} = sprintf('%s %ik %iOhm',to_plot{index,1},to_plot{index,2},to_plot{index,3});
+    legend_info{index} = sprintf('%iV %s %ik %iOhm',to_plot{index,1},to_plot{index,2},to_plot{index,3},to_plot{index,4});
 end
 
 set_figure_style();
